43a3b7c689d2c0534809f8a485947050e10071d1
JB <46815338+officialtech@users.noreply.github.com>
2023-03-23 00:12:20 +0530

    updated env and client id and secret (#1)

--- apps/gc/constant.py
+++ apps/gc/constant.py
@@ -5,7 +5,7 @@ from decouple import config
 REDIRECT_URL = """http://localhost:5000"""
 SCOPES = """https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/calendar https://www.googleapis.com/auth/calendar.events https://www.googleapis.com/auth/calendar.events.readonly https://www.googleapis.com/auth/calendar.readonly https://www.googleapis.com/auth/calendar.settings.readonly"""
 
-RAW_AUTH_URL = f"""https://accounts.google.com/o/oauth2/v2/auth/oauthchooseaccount?redirect_uri={REDIRECT_URL}&prompt=consent&response_type=code&client_id={config('CLIENT_ID')}&scope={SCOPES}&access_type=offline&service=lso&o2v=2&flowName=GeneralOAuthFlow"""
+RAW_AUTH_URL = f"""https://accounts.google.com/o/oauth2/v2/auth/oauth?redirect_uri={REDIRECT_URL}&prompt=consent&response_type=code&client_id={config('CLIENT_ID')}&scope={SCOPES}&access_type=offline"""
 
 AUTH_URL = RAW_AUTH_URL.replace(' ', '%20')
 
--- apps/hubspot/main_handler.py
+++ apps/hubspot/main_handler.py
@@ -15,13 +15,14 @@ def generate_tokens(request):
     """generating auth tokens using auth code """
     payload = f"""code={request.headers.get("code")}&\
                 grant_type=authorization_code&\
-                client_id={config("CLIENT_ID")}&\
-                client_secret={config("CLIENT_SECRET")}&\
+                client_id={config("CLIENT_ID_HUBSPOT")}&\
+                client_secret={config("CLIENT_SECRET_HUBSPOT")}&\
                 redirect_uri={config("REDIRECT_URI")}""".replace(" ", "")
     tokens = requests.request(
         method="POST",
         url=TOKEN_URL,
         headers=HEADERS,
         data=payload,
+        timeout=20,
     )
     return tokens.json()
