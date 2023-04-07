db99b3d2dac4518c29d10ddc70cecf44f3eab08d
JB <46815338+officialtech@users.noreply.github.com>
2023-04-07 23:04:09 +0530

    updated hubspot oauth URL

--- apps/hubspot/auth.py
+++ apps/hubspot/auth.py
@@ -6,7 +6,11 @@ from apps.hubspot.constants import HS_SCOPES
 
 HS_SCOPES = HS_SCOPES.replace(" ", "%20")
 
-AUTH_URI = f"""https://app.hubspot.com/oauth/authorize?client_id={config("CLIENT_ID")}\
-&redirect_uri={config("REDIRECT_URI")}\
-&scope={HS_SCOPES}"""
+# old
+# AUTH_URI = f"""https://app.hubspot.com/oauth/authorize?client_id={config("CLIENT_ID")}\
+# &redirect_uri={config("REDIRECT_URI")}\
+# &scope={HS_SCOPES}"""
+
+# new
+AUTH_URI = """https://app.hubspot.com/oauth/authorize?client_id=5c5d80a6-a551-4081-8a42-91ee10a86993&redirect_uri=https://saaswinlvl.bu-book.com/hubspot&scope=oauth%20crm.objects.contacts.read%20crm.schemas.contacts.read"""
 
